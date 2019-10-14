7a9a06746d2d011483f6333f13fbe6a7c7bf0ab9
cuboktahedron <cuboktahedron@gmail.com>
2019-10-14 20:16:49 +0900

    add Swap_hv

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,10 @@ import qualified Hengen.Lang.Graph as Graph
 main :: IO ()
 
 main = do
-  test2
+  test3
 
 test1 = Graph.execHengenPixel "minimal.dat"
 test2 = Graph.execHengenPixel "border.dat"
+test3 = Graph.execHengenPixel "swap_hv.dat"
+
   
